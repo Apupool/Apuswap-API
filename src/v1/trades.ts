3d39f1aad04d90c30c8cdd501efdc67e4532a51c
Apupool <82410310+Apupool@users.noreply.github.com>
2021-04-12 22:58:12 -0400

    Update trades.ts

--- src/v1/trades.ts
+++ src/v1/trades.ts
@@ -4,7 +4,7 @@ import { getTrades } from './_shared'
 import { createSuccessResponse, createBadRequestResponse, createServerErrorResponse } from '../utils/response'
 
 export const handler: APIGatewayProxyHandler = async event => {
-  if (!event.pathParameters?.pair || !/^ETH_0x[0-9a-fA-F]{40}$/.test(event.pathParameters.pair)) {
+  if (!event.pathParameters?.pair || !/^ZNN_0x[0-9a-fA-F]{40}$/.test(event.pathParameters.pair)) {
     return createBadRequestResponse()
   }
 
